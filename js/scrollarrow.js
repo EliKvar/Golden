5843c9fa6f6c00afa503fb37a79ff883fab8c2db
EliKvar <46699047+EliKvar@users.noreply.github.com>
2019-03-09 20:50:23 -0700

    Finished my html web page. Added a js folder. just need to add the nav bar.

---
+++ js/scrollarrow.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+ $(function() {
+    $('.scroll-down').click (function() {
+      $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
+      return false;
+    });
+  });
